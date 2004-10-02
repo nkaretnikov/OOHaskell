0c62be3d1d09cc7ea6cc40116f300547e9d4a872
oleg <unknown>
2004-10-02 04:31:26 +0000

    [project @ 2004-10-02 04:31:26 by oleg]
    better syntax

--- simple-st.hs
+++ simple-st.hs
@@ -17,6 +17,12 @@
 -- Sec 3.1 Classes and objects
 
 
+-- Using objects with mutable fields in the context of ST monad
+-- Notable point: our labels and objects must depend on the
+-- type 's' that represents a thread. This is natural:
+-- once an object is instantiated, it can be used only in the same
+-- thread that created that. In that sense, objects are a generalization
+-- of a STRef.
 
 
 module SimpleSTObj where
@@ -34,17 +40,30 @@ import Data.STRef
 import Data.IORef
 import Control.Monad.ST
 
-infixr 9 #
-m # field = m .!. field
-
 -- First, declare the labels.
 -- We use proxies as of HList/Label4.hs
+-- We explicitly carry the ST thread type 's' in the type of the label
 
-data MutableX s; mutableX (_::ST s t)  = proxy::Proxy (MutableX s)
-data GetX s;     getX (_::ST s t)      = proxy::Proxy (GetX s)
-data MoveD s;    moveD (_::ST s t)     = proxy::Proxy (MoveD s)
-data OffsetX s;  offsetX (_::ST s t)   = proxy::Proxy (OffsetX s)
+data MutableX; mutableX (_::st s t)  = proxy::Proxy (s, MutableX)
+data GetX;     getX (_::st s t)      = proxy::Proxy (s, GetX)
+data MoveD;    moveD (_::st s t)     = proxy::Proxy (s, MoveD)
+data OffsetX;  offsetX (_::st s t)   = proxy::Proxy (s, OffsetX)
 
+-- Our records are wrapped up in STRecord that carries the 's'
+-- type (as a phantom type)
+
+newtype STRecord s r = STRecord r
+
+-- We modify the field applicator '#' to extract 's' from the object
+-- and pass it to the label constructor. This will guarantee that
+-- the desirable label has the same thread type 's' as that of the object.
+-- This convention also makes _using_ of ST objects look precisely the
+-- same as _using_ of the IORef objects.
+
+infixr 9 #
+m@(STRecord r) # field = r .!. (field m)
+
+blessST (_::st s a) (r::r)  = (STRecord r) :: STRecord s r
 
 -- test that hLookupByHNat works even on lists that contain ST s t
 -- with s being the quantified (eventually) variable
@@ -67,12 +86,11 @@ method_offset x origin = do{v<-readSTRef x; return$ v - origin}
 class_pointST x_init
   = do
       x <- newSTRef x_init
-      let me = readSTRef x -- this is just the most weird way of getting 's'
-      return $ 
-	        (mutableX me) .=. x
-	    .*. (getX me)     .=. readSTRef x
-            .*. (offsetX me)  .=. method_offset x x_init
-            .*. (moveD me)    .=. method_move x
+      return $ blessST x $
+	        (mutableX x) .=. x
+	    .*. (getX x)     .=. readSTRef x
+            .*. (offsetX x)  .=. method_offset x x_init
+            .*. (moveD x)    .=. method_move x
             .*. emptyRecord
 
 printST buf val = 
@@ -91,16 +109,36 @@ testp3 = do
 	  print "OK"
 -}
 
+-- Note that the syntax remains the same as in the simple.st file
 testoST1 = 
     let 
       test :: ST s String
       test = do
 	      printbuf <- newSTRef ""
-	      let me = readSTRef printbuf
 	      p <- class_pointST 1
-	      (p # (getX me)) >>= (printST printbuf)
-	      p # (moveD me) $ 2
-	      (p # (getX me)) >>= (printST printbuf)
-	      (p # (offsetX me)) >>= (printST printbuf)
+	      p # getX >>= (printST printbuf)
+	      p # moveD  $ 2
+	      p # getX >>= (printST printbuf)
+	      p # offsetX >>= (printST printbuf)
+	      readSTRef printbuf
+    in runST test
+
+-- We now show an example of two objects, of reading something from
+-- one object and putting into the other.
+-- Both objects are within the same ST thread.
+
+testoST2 = 
+    let 
+      test :: ST s String
+      test = do
+	      printbuf <- newSTRef ""
+	      p1 <- class_pointST 1
+	      p2 <- class_pointST 10
+	      p1 # getX >>= (printST printbuf)
+	      p2 # getX >>= (printST printbuf)
+	      -- reading from one and putting into the other
+	      p1 # getX >>= (p2 # moveD) 
+	      p1 # getX >>= (printST printbuf)
+	      p2 # getX >>= (printST printbuf)
 	      readSTRef printbuf
     in runST test
