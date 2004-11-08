ab4c444d6e60cb53292b883a6053e0708d8eb0cb
ralf <unknown>
2004-11-08 13:14:31 +0000

    [project @ 2004-11-08 13:14:31 by ralf]

--- term.hs
+++ term.hs
@@ -1,10 +1,12 @@
 {-# OPTIONS -fglasgow-exts #-}
 {-# OPTIONS -fallow-undecidable-instances #-}
+{-# OPTIONS -fallow-overlapping-instances #-}
 
 module Term where
 
-class Term c a | c->a
+import TypeCastGeneric2
 
+-- Term syntax
 newtype Lit a = Lit a
 newtype Inc a = Inc a
 newtype IsZ a = IsZ a
@@ -13,6 +15,8 @@ newtype Fst a = Fst a
 newtype Snd a = Snd a
 newtype Pair a b = Pair (a,b)
 
+-- Typed terms
+class Term c a | c->a
 instance Term (Lit Int) Int
 instance Term a Int => Term (Inc a) Int
 instance Term a Int => Term (IsZ a) Bool
@@ -21,9 +25,8 @@ instance (Term t1 a, Term t2 b) => Term (Pair t1 t2) (a,b)
 instance Term t (a,b) => Term (Fst t) a
 instance Term t (a,b) => Term (Snd t) b
 
-class Term t a => Eval t a | t->a where
-     eval:: t -> a
-
+-- Term evaluation
+class Term t a => Eval t a | t -> a where eval:: t -> a
 instance Term (Lit a) a => Eval (Lit a) a where eval (Lit i) = i
 instance Eval t Int => Eval (Inc t) Int where eval (Inc t) = eval t + 1
 instance Eval t Int => Eval (IsZ t) Bool where eval (IsZ t) = eval t == 0
@@ -34,9 +37,29 @@ instance (Eval t1 a, Eval t2 b) => Eval (Pair t1 t2) (a,b) where
 instance Eval t (a,b) => Eval (Fst t) a where eval (Fst t) = fst (eval t)
 instance Eval t (a,b) => Eval (Snd t) b where eval (Snd t) = snd (eval t)
 
+-- A sample term
 t1 = If ( (IsZ (Fst (Pair (Lit (0::Int), Lit (1::Int)))))
         , (Inc (Fst (Pair (Lit (0::Int), Lit (1::Int)))))
         , (Inc (Snd (Pair (Lit (0::Int), Lit (1::Int))))) )
 
-
+-- A sample evaluation
 et1 = eval t1
+
+{-
+
+Stephanie proposed to find out whether we can have 
+a catch-all case such that the type index of a term
+coincides with the untyped term type.
+
+Once again, TypeCast is the key asset!
+
+-}
+
+instance TypeCast a b => Term a b
+instance (TypeCast a b, Term a b) => Eval a b where eval = typeCast
+
+t2 = If ( (IsZ (Fst (Pair (0::Int,1::Int))))
+        , (Inc (Fst (Pair (42::Int,1::Int))))
+        , (Inc (Snd (Pair (88::Int,1::Int)))) ) 
+
+et2 = eval t2
