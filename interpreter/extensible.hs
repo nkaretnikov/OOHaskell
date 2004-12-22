a3244c03a831043edcc44d5a747115677610982e
ralf <unknown>
2004-12-22 17:28:05 +0000

    [project @ 2004-12-22 17:28:05 by ralf]

--- interpreter/extensible.hs
+++ interpreter/extensible.hs
@@ -1,9 +1,29 @@
+{-
+ 
+OOHaskell (C) 2004, Oleg Kiselyov, Ralf Laemmel, Keean Schupke
+ 
+http://homepages.cwi.nl/~ralf/OOHaskell/
+
+Incidentally, this example does *not* use OOHaskell. It rather deals
+with some nice OOP programming bit that is readily enabled in Haskell
+even without OOHaskell skills. We use Haskell's type classes to
+program in a style of extensible datatypes. The example is about a
+tiny interpreter, which we could extend by new syntax, whenever
+necessary.
+
+Also see file "extensible.hs".
+
+-}
+
+
 import Control.Monad.State
 import Data.FiniteMap
 import Data.Maybe
 
+-- Class for expression syntax
 class Exp x
 
+-- Expression syntax so far; could be extended
 data  Zero                       = Zero
 data  Exp x          => Succ x   = Succ x
 data  (Exp x, Exp y) => Then x y = Then x y
@@ -17,7 +37,13 @@ instance                   Exp Var
 instance Exp x          => Exp (Assign x)
 
 type  Id  = String
+
+
+-- Semantic domain
 type  Val = Int
+
+
+-- The monadic interpreter function
 
 class Exp x => Interpret x
  where
@@ -49,6 +75,8 @@ instance Interpret x => Interpret (Assign x)
        () <- put m'
        return v
 
+
+-- Test harness
 main = print $ fst $ runState (interpret
          ( (Assign "a" (Succ Zero))
            `Then`
--- interpreter/nonextensible.hs
+++ interpreter/nonextensible.hs
@@ -1,7 +1,30 @@
+{-
+ 
+OOHaskell (C) 2004, Oleg Kiselyov, Ralf Laemmel, Keean Schupke
+ 
+http://homepages.cwi.nl/~ralf/OOHaskell/
+
+Incidentally, this example does *not* use OOHaskell. It rather deals
+with some nice OOP programming bit that is readily enabled in Haskell
+even without OOHaskell skills. Incidentally, this file does *not* even
+show that nice OOP bit, but it rather shows how we normally program in
+Haskell, and we would appreciate extensibility. That is, this file
+shows a tiny interpreter, which is extensible in the dimension of the
+semantic domain (because we use monads), but nonextensible in the
+dimension of syntax (because Haskell's datatypes are not exactly
+extensible as of today).
+
+Also see file "extensible.hs".
+
+-}
+
+
 import Control.Monad.State
 import Data.FiniteMap
 import Data.Maybe
 
+
+-- Expression syntax
 data Exp = Zero
          | Succ Exp
          | Then Exp Exp
@@ -9,8 +32,13 @@ data Exp = Zero
          | Assign Id Exp
 
 type Id  = String
+
+
+-- Semantic domain
 type Val = Int
 
+
+-- The monadic interpreter function
 interpret :: Exp -> State (FiniteMap Id Val) Val
 interpret Zero         = return 0
 interpret (Succ x)     = interpret x >>= return . (+) 1
@@ -24,6 +52,8 @@ interpret (Assign i x) =
      () <- put m'
      return v
 
+
+-- Test harness
 main = print $ fst $ runState (interpret
          ( (Assign "a" (Succ Zero))
            `Then`
