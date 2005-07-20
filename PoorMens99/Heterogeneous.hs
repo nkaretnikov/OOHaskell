a89fe819a41778bf5ace0cbd86ea2bc50dba6efa
ralf <unknown>
2005-07-20 20:00:00 +0000

    [project @ 2005-07-20 20:00:00 by ralf]

---
+++ PoorMens99/Heterogeneous.hs
@@ -0,0 +1,42 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+{-
+
+We modify the basic Haskell 98 solution 
+to use heteregenous list instead of explicit tagging.
+
+-}
+
+module Heterogeneous where
+
+import MainGhcGeneric1
+import Shape
+import Circle
+import Rectangle
+
+
+-- A type code for the body of the loop
+data ScribbleBody
+
+
+-- The actual loop body
+instance Shape s => Apply ScribbleBody s (IO ())
+ where
+  apply _ x = 
+     do
+        draw x
+        draw (rMoveTo 100 100 x)
+
+
+-- Weirich's / Rathman's test case
+
+main =
+      do
+         -- Handle the shapes polymorphically
+         let scribble =  HCons (rectangle 10 20 5 6)
+                        (HCons (circle 15 25 8)
+                         HNil)
+         hMapM_ (undefined::ScribbleBody) scribble
+
+         -- Handle rectangle-specific instance
+         draw $ setWidth 30 (rectangle 0 0 15 15)
---
+++ PoorMens99/Inference.hs
@@ -0,0 +1,25 @@
+module Inference where
+
+import Shape
+import Circle
+import Rectangle
+
+
+
+-- Weirich's / Rathman's test case
+
+main =
+      do
+         -- Handle the shapes polymorphically
+         let scribble = [ Left  (rectangle 10 20 5 6)
+                        , Right (circle 15 25 8)
+                        ]
+         mapM_ (either scribbleBody scribbleBody) scribble
+
+         -- Handle rectangle-specific instance
+         draw $ setWidth 30 (rectangle 0 0 15 15)
+ where
+   scribbleBody x =
+     do
+         draw x
+         draw (rMoveTo 100 100 x) 
