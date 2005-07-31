3a3910c3e001519951bf77f957708d0b74abaaa9
ralf <unknown>
2005-07-31 23:14:52 +0000

    [project @ 2005-07-31 23:14:52 by ralf]

--- ShapesEither.hs
+++ ShapesEither.hs
@@ -8,16 +8,22 @@
 -- Haskell's overlooked object system
 
 A variation on the shapes example. We encode subtyping through
-particular intersection types.  That is, we (encode as if we) use a
-TIC (i.e., a union or variant type with unqiue summands) whose OO
-interface is the intersection of the summand types. We use the term
-"union intersection types" (UI types) to denote these types. The
-strong aspect of this encoding is that down-casts remain possible and
-no nominal up-casts are required from the programmer. Down-casts are
-completely type-safe; we can only attempt to cast to types that are
-part of the union. In order to make all this work, we need to define
-several new type-level functions. These new functions may be
-eventually moved to the HList or the OOHaskell libraries.
+open-ended normal disjoint unions. When we attempt record look-up, we
+simply require that both summands admit the look-up since we could
+find that any summand is actually inhabited at the value level. Two
+strengths of this approach are these: it readily allows for
+down-casts, and and no nominal up-casts are required from the
+programmer. Down-casts are completely type-safe; we can only attempt
+to cast to types that are part of the union. In order to make all this
+work, we need to define a few new type-level functions. These new
+functions may eventually be moved to the HList or the OOHaskell
+libraries.
+
+The given encoding and its use in the shapes example is somewhat
+inefficient in so far that the depth of the sum resembles the length
+of the subtype-polymorphic list. It should be clear that the
+construction of the sum type and its inhabitation (through the helper
+eitherCons) was easily optimised to observe the existence of summands.
 
 -}
 
@@ -42,11 +48,10 @@ main =
        s3 <- mfix (square (35::Int) (45::Int) (8::Int))
 
        -- We could have used a vararg function.
-       let scribble = union'inter (HCons s2 
-			          (HCons s1
-                                  (HCons s2
-                                  (HCons s3
-                                   HNil))))
+       let scribble =  eitherCons s2
+                      (eitherCons s1
+                      (eitherCons s2
+                      [s3] ))
 
        -- Iterate through the array
        -- and handle shapes polymorphically.
@@ -70,90 +75,110 @@ main =
              scribble
 
 
--- Construct normal list from HList
+-- Normal list cons combined with embedding into a sum
 
-class UI l r | l -> r
-  where
-    union'inter :: l -> [r]
-
-
---
--- The following encoding does not care about type doubles.
--- Hence, the depth of the sum equals the number of list elements.
--- This could be clearly optimised, if necessary.
---
-
-instance UI (HCons e HNil) e
-  where 
-    union'inter (HCons e HNil) = [e]
-
-instance UI (HCons e2 t) r => 
-         UI (HCons e1 (HCons e2 t)) (Either e1 r)
-  where
-    union'inter (HCons h t) = let t' = union'inter t
-			      in (Left h) : (map Right t')
+eitherCons :: x -> [y] -> [Either x y]
+eitherCons x ys = Left x : map Right ys
 
 
---
--- We also instantiate the look-up (HasField) class for records.
--- Here we essentially compute the intersection.
--- This is literally expressed in the instance for Either.
---
+-- We instantiate the look-up (HasField) class for records.
+-- We assure that the constructed unions form reasonable intersection type.
 
-instance (HasField l a v, HasField l b v) 
-       => HasField l (Either a b) v 
+instance (HasField l x v, HasField l y v) 
+       => HasField l (Either x y) v 
  where
-  hLookupByLabel l (Left a)  =  hLookupByLabel l a
-  hLookupByLabel l (Right b) =  hLookupByLabel l b
+  hLookupByLabel l (Left x)  =  hLookupByLabel l x
+  hLookupByLabel l (Right y) =  hLookupByLabel l y
 
 
--- Down-cast a value of an UI type to a summand type
+-- Down-cast a value of a union type to a summand type.
+-- Make sure that the summand type occurs once at least.
+-- Rely on a right associativity assumption for unions.
 
-class DownCast f t
+class DownCast u s
   where
-    downCast :: f -> Maybe t
+    downCast :: u -> Maybe s
 
-instance (DownCastEither bf a b t, TypeEq a t bf) 
-      =>  DownCast (Either a b) t
+instance (DownCastEither b x y s, TypeEq x s b) 
+      =>  DownCast (Either x y) s
   where
-    downCast = downCastEither (undefined::bf)
+    downCast = downCastEither (undefined::b)
 
-instance (DownCastOne bf a t, TypeEq a t bf)
-      =>  DownCast a t
+instance TypeCast x s
+      => DownCast x s
   where
-    downCast = downCastOne (undefined::bf)
+    downCast = Just . typeCast
 
 
--- Downcast a type, as is.
+-- Downcast a sum-typed value.
+-- Make sure that the summand type occurs once at least.
 
-class DownCastOne bf a t
+class DownCastEither b x y s
   where
-    downCastOne :: bf -> a -> Maybe t
+    downCastEither :: b -> Either x y -> Maybe s
 
-instance TypeCast a t
-      => DownCastOne HTrue a t
+instance DownCast y s
+      => DownCastEither HFalse x y s
   where
-    downCastOne _ = Just . typeCast
+    downCastEither _ (Left x)  = Nothing
+    downCastEither _ (Right y) = downCast y
 
-instance DownCastOne HFalse a t
+instance (TypeCast x s, DownCastTotal y s)
+      =>  DownCastEither HTrue x y s
   where
-    downCastOne _ = const Nothing
+    downCastEither _ (Left x)  = Just (typeCast x)
+    downCastEither _ (Right y) = downCastTotal y
 
 
--- Downcast a sum-typed value
+-- Down-cast a value of a union type to a summand type.
+-- Do not insist on the summand type to be present.
 
-class DownCastEither bf a b t
+class DownCastTotal u s
   where
-    downCastEither :: bf -> Either a b -> Maybe t
+    downCastTotal :: u -> Maybe s
 
-instance (DownCast b t, TypeCast a t)
-      =>  DownCastEither HTrue a b t
+instance (DownCastEitherTotal b x y s, TypeEq x s b) 
+      =>  DownCastTotal (Either x y) s
   where
-    downCastEither _ (Left x)   = Just (typeCast x)
-    downCastEither _ (Right x') = downCast x'
+    downCastTotal = downCastEitherTotal (undefined::b)
 
-instance DownCast b t
-      => DownCastEither HFalse a b t
+instance (TypeCastBool b x s, TypeEq x s b)
+      =>  DownCastTotal x s
   where
-    downCastEither _ (Left x)   = Nothing
-    downCastEither _ (Right x') = downCast x'
+    downCastTotal = typeCastBool (undefined::b)
+
+
+-- Type-level type cast; return Nothing if all fails.
+
+class TypeCastBool b x y 
+  where
+    typeCastBool :: b -> x -> Maybe y
+
+instance TypeCast x y
+      => TypeCastBool HTrue x y
+  where
+    typeCastBool _ = Just . typeCast
+
+instance TypeCastBool HFalse x y
+  where
+    typeCastBool _ = const Nothing
+
+
+-- Downcast a sum-typed value.
+-- Do not insist on the summand type to be present.
+
+class DownCastEitherTotal b x y s
+  where
+    downCastEitherTotal :: b -> Either x y -> Maybe s
+
+instance DownCastTotal y s
+      => DownCastEitherTotal HFalse x y s
+  where
+    downCastEitherTotal _ (Left x)  = Nothing
+    downCastEitherTotal _ (Right y) = downCastTotal y
+
+instance (TypeCast x s, DownCastTotal y s)
+      =>  DownCastEitherTotal HTrue x y s
+  where
+    downCastEitherTotal _ (Left x)  = Just (typeCast x)
+    downCastEitherTotal _ (Right y) = downCastTotal y
