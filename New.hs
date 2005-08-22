16d575ee04c9f3b28ad0cc40869ea28cb955d9fc
ralf <unknown>
2005-08-22 23:44:26 +0000

    [project @ 2005-08-22 23:44:26 by ralf]

--- New.hs
+++ New.hs
@@ -5,7 +5,7 @@
 -- (C) 2004-2005, Oleg Kiselyov & Ralf Laemmel
 -- Haskell's overlooked object system
 
--- Safe MFix to prevent staging errors
+-- Safe mfix to prevent staging errors
 -- NotFixed *data* constructor is not exported
 
 -}
@@ -23,8 +23,10 @@ import Record
 newtype NotFixed a = NotFixed a
 
 
+--
 -- Only the following two primitives deconstruct the NotFixed term.
 -- These primitives constitute the security kernel.
+--
 
 new :: (NotFixed (Record a) -> IO (NotFixed (Record a))) 
     -> IO (Record a)
@@ -32,9 +34,11 @@ new :: (NotFixed (Record a) -> IO (NotFixed (Record a)))
 new f = mfix f >>= (\(NotFixed a) -> return a)
 
 
+--
 -- Note that f has not a monadic type.
 -- So during the execution of f, effects are not possible.
 -- Except non-termination.
+--
 
 construct :: NotFixed (Record a)
           -> (Record a -> Record b)
@@ -45,12 +49,16 @@ construct (NotFixed self) f = returnIO $ NotFixed (f self)
 
 --
 -- We also need a version that takes into account a super.
+-- For multiple inheritance/object composition we would need yet another version.
+-- It's straightforward to use a type class to this end.
+-- Working title: "constructWithParents".
 --
 
 constructWithSuper :: NotFixed (Record super)
                    -> NotFixed (Record self)
                    -> (Record super -> Record self -> Record this)
                    -> IO (NotFixed (Record this))
+
 constructWithSuper (NotFixed super)
                    (NotFixed self)
 		   f
