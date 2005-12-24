21325fa832bd2ba5e336ad6e44ef4b985504b78b
ralf <unknown>
2005-12-24 09:51:27 +0000

    [project @ 2005-12-24 09:51:27 by ralf]

---
+++ Family.hs
@@ -0,0 +1,39 @@
+{-# OPTIONS -fglasgow-exts #-}
+{-# OPTIONS -fallow-undecidable-instances #-}
+{-# OPTIONS -fallow-overlapping-instances #-}
+{-# OPTIONS -fno-monomorphism-restriction #-}
+
+module Family where
+
+import OOHaskell
+
+
+data Foo; foo = proxy::Proxy Foo
+data Bar; bar = proxy::Proxy Bar
+data Baz; baz = proxy::Proxy Baz
+
+
+family1 =  foo .=. fooClass
+       .*. emptyRecord
+
+fooClass x self =
+  returnIO
+    $  bar .=. putStr ""
+   .*. baz .=. x
+   .*. emptyRecord
+
+
+client family =
+  do
+     o1 <- mfix ((family # foo) True)
+     o1 # bar
+--   family monomorphic
+--     o2 <- mfix ((family # foo) "True")
+--     o2 # bar
+     o3 <- mfix (fooClass True)
+     o3 # bar
+     o4 <- mfix (fooClass "True")
+     o4 # bar
+
+main = do 
+          client family1
