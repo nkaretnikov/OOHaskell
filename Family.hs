ffaf03afd6ddcace39e534b57044454e5143bc16
oleg <unknown>
2005-12-29 10:59:29 +0000

    [project @ 2005-12-29 10:59:29 by oleg]
    changes

--- Family.hs
+++ Family.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS -fglasgow-exts #-}
 {-# OPTIONS -fallow-undecidable-instances #-}
 {-# OPTIONS -fallow-overlapping-instances #-}
-{-# OPTIONS -fno-monomorphism-restriction #-}
+-- {-# OPTIONS -fno-monomorphism-restriction #-}
 
 module Family where
 
@@ -11,6 +11,7 @@ import OOHaskell
 data Foo; foo = proxy::Proxy Foo
 data Bar; bar = proxy::Proxy Bar
 data Baz; baz = proxy::Proxy Baz
+data Qux; qux = proxy::Proxy Qux
 
 
 family1 =  foo .=. fooClass
@@ -18,9 +19,17 @@ family1 =  foo .=. fooClass
 
 fooClass x self =
   returnIO
-    $  bar .=. putStr ""
+    $  bar .=. (putStrLn $ show $ (self # baz))
    .*. baz .=. x
    .*. emptyRecord
+
+fooClass2 x y self =
+  do
+  super <- fooClass x self
+  returnIO
+    $  qux .=. y
+   .*. (bar .=. (putStrLn ("fooClass2" ++ (show (self # qux))) >> super # bar)
+	.<. super)
 
 
 client1 family =
@@ -43,6 +52,46 @@ client2 fooClass =
 --     o6 <- mfix (fooClass "True")
 --     o6 # bar
 
+family2 fooself =  foo .=. (\x -> fooClass x fooself)
+       .*. emptyRecord
+
+-- To compile the latter, we should NOT have -fno-monomorphism-restriction
+-- Monomorphism restriction is good here!
+client1' =
+  do
+     putStrLn "client1'"
+     o1 <- mfix (\fooself -> ((family2 fooself) # foo) True)
+     o1 # bar
+     o2 <- mfix (\fooself -> ((family2 fooself) # foo) "True")
+     o2 # bar
+
+-- Note that when it comes to class constructors, we should try to find
+-- the greatest point (the lattice join? or is it meet?) whatever, 
+-- the greatest point
+-- BTW, in the name of consLub, was Lub correct? should it have been
+-- consGlb (greater lower bound)?
+-- Then what we search for in here is lub!
+-- The addition of 'qux' could be made generic -- it is the opposite
+-- of narrow. But we don't care of genericity at this point...
+list'of'foos self =
+	   [\x -> (fooClass x self) >>= 
+	           (\r -> return ( (qux .=. undefined) .*. r)),
+	    \x -> fooClass2 x x self]
+
+-- instead of passing the class constructor, we pass an index...
+client2' n =
+       do
+       putStrLn "client2'"
+       o1 <- mfix (\self -> ((list'of'foos self) !! n) True)
+       o1 # bar
+       o2 <- mfix (\self -> ((list'of'foos self) !! n) "True")
+       o2 # bar
+
+
+
 main = do 
           client1 family1
           client2 fooClass
+	  client1'
+	  client2' 0
+	  client2' 1
