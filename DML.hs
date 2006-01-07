041049e67929b651b64a36514b76f61308ad2fe8
oleg <unknown>
2006-01-07 04:15:56 +0000

    [project @ 2006-01-07 04:15:56 by oleg]
    improvement to DML

--- DML.hs
+++ DML.hs
@@ -36,17 +36,18 @@ lit (x::Int) self =
 
 -- Add expressions
 
-add (l::l) (r::r) self = 
+-- no type annotations are needed here
+add l r self = 
   do
      lRef <- newIORef l
      rRef <- newIORef r
      returnIO $  left  .=. property lRef
              .*. right .=. property rRef
              .*. print .=. ( do 
-                                (l::l) <- get (self # left)
+                                l <- get (self # left)
                                 l # print
                                 putStr "+"
-                                (r::r) <- get (self # right)
+                                r <- get (self # right)
                                 r # print
                            )
              .*. emptyRecord                                
@@ -181,7 +182,59 @@ test3 = do
            Prelude.print "test3 done."
 
 
+-- Add expressions
+
+p `setN` x = (snd p) (narrow x)
+
+-- make sure that when we put 'x' into an IORef, we can store into that IORef
+-- both x and y (after narrowing)
+-- This is better than C++: we do not need to declare the type of the field.
+-- we need to specify which values we wish to store there.
+-- BTW, any supertype of x and y can also be stored there, without additional
+-- declaration. So, this function is needed only if x is not a supertype
+-- of y
+thisandalso x y = fst (lubNarrow x y)
+
+test4 = do
+           -- Construct and manipulate literal object
+           lit1 <- mfix (lit 42)
+           lit2 <- mfix (lit 37)
+           lit3 <- mfix (lit 88)
+           lit1 # print; putStr "\n"
+           lit2 # print; putStr "\n"
+           lit3 # print; putStr "\n"
+	   let add1f self :: IO r1 = 
+			     (add (thisandalso lit1 (undefined::r1))
+			          (thisandalso lit2 (undefined::r1)) self)
+	   (add1 :: r) <- mfix add1f
+	   (add2 :: r) <- (mfix (add (narrow add1) (narrow lit3)))
+	   add1 # print; putStr "\n"
+	   add2 # print; putStr "\n"
+           -- Apparant type change
+           l <- get (add2 # left)    -- heavy tree
+           r <- get (add2 # right)   -- light tree
+           setN (add2 # left) r      -- swap subtrees
+           setN (add2 # right) l      -- swap cont'd
+           add2 # print; putStr "\n"
+
+           -- Type change through setters
+           setN (add2 # left)  lit1 -- Ooh!
+           setN (add2 # right) lit1 -- Eeh!
+           add2 # print; putStr "\n"
+           Prelude.print "test4 done."
+
 main = do
           test1
           test2
           test3
+          test4
+
+{-
+tx () = do
+     x <- newIORef (?b # print)
+     y <- newIORef undefined
+     lit1 <- mfix (lit 42)
+     writeIORef y (narrow lit1)
+     return (x,y)
+     --Prelude.print "test4 done."
+-}
