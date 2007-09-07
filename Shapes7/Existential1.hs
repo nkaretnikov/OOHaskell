bc0514cdf86190bb709d1a8198ce3b82086b7d95
rlaemmel <rlaemmel@gmail.com>
2007-09-07 16:49:21 +0000

    Committed from the Makefile

--- Shapes7/Existential1.hs
+++ Shapes7/Existential1.hs
@@ -15,41 +15,36 @@ import Rectangle
 
 -- An `existential' envelope for shapes
 
-data OpaqueShape = 
-    OpaqueShape{sh_moveTo  :: Int -> Int -> OpaqueShape,
-		sh_rMoveTo :: Int -> Int -> OpaqueShape,
-		sh_draw    :: IO ()}
+data AbstractShape = 
+     AbstractShape { moveToA  :: Int -> Int -> AbstractShape
+                   , rMoveToA :: Int -> Int -> AbstractShape
+                   , drawA    :: IO ()}
 
 
 -- Opaque shapes are shapes
 
-instance Shape OpaqueShape 
+instance Shape AbstractShape 
  where
-  moveTo x y    s = sh_moveTo s x y
-  rMoveTo dx dy s = sh_rMoveTo s dx dy
-  draw          s = sh_draw s
+  moveTo x y    s = moveToA s x y
+  rMoveTo dx dy s = rMoveToA s dx dy
+  draw          s = drawA s
 
 
 -- The following two injections could be collected in a type class
 
-inj_rectangle rect = 
-    OpaqueShape{sh_moveTo  = \x y   -> inj_rectangle $ moveTo x y rect,
-		sh_rMoveTo = \dx dy -> inj_rectangle $ rMoveTo dx dy rect,
-		sh_draw    = draw rect}
+abstractShape s =
+  AbstractShape { moveToA  = \x y   -> abstractShape $ moveTo x y s
+                , rMoveToA = \dx dy -> abstractShape $ rMoveTo dx dy s
+                , drawA    = draw s }
 
-
-inj_circle circ = 
-    OpaqueShape{sh_moveTo  = \x y   -> inj_circle $ moveTo x y circ,
-		sh_rMoveTo = \dx dy -> inj_circle $ rMoveTo dx dy circ,
-		sh_draw    = draw circ}
 
 -- Weirich's / Rathman's test case
 
 main =
       do
          -- Handle the shapes polymorphically
-         let scribble = [ inj_rectangle (rectangle 10 20 5 6)
-                        , inj_circle (circle 15 25 8)
+         let scribble = [ abstractShape (rectangle 10 20 5 6)
+                        , abstractShape (circle 15 25 8)
                         ]
          mapM_ ( \x -> 
                    do
